03a68f8b3cfce73f099ca45731fab133087bb285
Puyodead1 <puyodead@proton.me>
2025-09-08 21:56:43 -0400

    [DVDServo2] Start implementing functions
    Implemented DPEC_Connect, DPEC_Disconnect, DPEC_StartLoggingService, DPEC_KillLoggingService, DPEC_AbortOperation

--- DvdServo2_0/main.cpp
+++ DvdServo2_0/main.cpp
@@ -1,8 +1,43 @@
 #include "net_dvdplay_aem_DPEC.h"
+#include <windows.h>
+
+typedef INT_PTR(WINAPI* DPEC_ConnectFunc)(void);
+typedef INT_PTR(WINAPI* DPEC_DisconnectFunc)(void);
+typedef INT_PTR(WINAPI* DPEC_StartLoggingFunc)(void);
+typedef INT_PTR(WINAPI* DPEC_KillLoggingFunc)(void);
+typedef INT_PTR(WINAPI* DPEC_AbortOperationFunc)(int);
+
+
+static JavaVM* g_java_vm = nullptr;
+static HMODULE g_dpec_handle = nullptr;
 
 JNIEXPORT jint JNICALL Java_net_dvdplay_aem_DPEC_Connect(JNIEnv* env, jclass clz)
 {
-	return 0;
+	env->GetJavaVM(&g_java_vm);
+	g_dpec_handle = LoadLibraryA("DPEC_API.dll");
+	if (g_dpec_handle == nullptr) {
+		return net_dvdplay_aem_DPEC_RESULT_CANTLOADDLL;
+	}
+
+	DPEC_ConnectFunc connectFunc = GetProcAddress(g_dpec_handle, "DPEC_Connect");
+	DPEC_DisconnectFunc disconnectAddress = GetProcAddress(g_dpec_handle, "DPEC_Disconnect");
+	DPEC_StartLoggingFunc startLoggingAddress = GetProcAddress(g_dpec_handle, "DPEC_StartLoggingService");
+	DPEC_KillLoggingFunc killLoggingAddress = GetProcAddress(g_dpec_handle, "DPEC_KillLoggingService");
+
+	(*startLoggingAddress)();
+	INT_PTR result = connectFunc();
+	if (result != 0)
+	{
+		(*disconnectAddress)();
+		(*killLoggingAddress)();
+		Sleep(100);
+		if (FreeLibrary(g_dpec_handle) == 0)
+		{
+			return net_dvdplay_aem_DPEC_RESULT_CANTFREEDLL;
+		}
+	}
+
+	return static_cast<jint>(result);
 }
 
 
@@ -20,7 +55,20 @@ JNIEXPORT void JNICALL Java_net_dvdplay_aem_DPEC_ReleaseGlobalRef(JNIEnv* env, j
 
 JNIEXPORT jint JNICALL Java_net_dvdplay_aem_DPEC_Disconnect(JNIEnv* env, jclass clz)
 {
-	return 0;
+	INT_PTR result = net_dvdplay_aem_DPEC_RESULT_CANTFREEDLL;
+
+	DPEC_DisconnectFunc disconnectFunc = GetProcAddress(g_dpec_handle, "DPEC_Disconnect");
+	DPEC_KillLoggingFunc killLoggingFunc = GetProcAddress(g_dpec_handle, "DPEC_KillLoggingService");
+
+	INT_PTR disconnectRet = disconnectFunc();
+	killLoggingFunc();
+
+	if (FreeLibrary(g_dpec_handle) != 0)
+	{
+		result = disconnectRet;
+	}
+
+	return static_cast<jint>(result);
 }
 
 
@@ -39,7 +87,9 @@ JNIEXPORT jint JNICALL Java_net_dvdplay_aem_DPEC_GetOpState(JNIEnv* env, jclass 
 // Appears unused
 JNIEXPORT jint JNICALL Java_net_dvdplay_aem_DPEC_AbortOperation(JNIEnv* env, jclass clz, jint var0)
 {
-	return 0;
+	DPEC_AbortOperationFunc abortFunc = (DPEC_AbortOperationFunc)GetProcAddress(g_dpec_handle, "DPEC_AbortOperation");
+	INT_PTR result = abortFunc(var0);
+	return static_cast<jint>(result);
 }
 
 
@@ -91,14 +141,14 @@ JNIEXPORT void JNICALL Java_net_dvdplay_aem_DPEC_AddLineToLog(JNIEnv* env, jclas
 	
 }
 
-// Appears unused
 JNIEXPORT void JNICALL Java_net_dvdplay_aem_DPEC_StartLoggingService(JNIEnv* env, jclass clz)
 {
-	
+	DPEC_StartLoggingFunc startLoggingFunc = GetProcAddress(g_dpec_handle, "DPEC_StartLoggingService");
+	startLoggingFunc();
 }
 
-// Appears unused
 JNIEXPORT void JNICALL Java_net_dvdplay_aem_DPEC_KillLoggingService(JNIEnv* env, jclass clz)
 {
-	
+	DPEC_KillLoggingFunc killLoggingFunc = GetProcAddress(g_dpec_handle, "DPEC_KillLoggingService");
+	killLoggingFunc();
 }
